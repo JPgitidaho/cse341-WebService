7cff558b614497de8b24ba4f49e36f1811e49363
JPgitidaho <juanichile2015@gmail.com>
2025-09-01 13:37:40 -0400

    actualization controllers

---
+++ controllers/lesson1.js
@@ -0,0 +1,12 @@
+const bugsRouter = (req, res) => {
+  res.send('Hello BUGS!')
+}
+
+const juanitaRouter = (req, res) => {
+  res.send('Hello World juanita!')
+}
+
+module.exports = { 
+ bugsRouter, 
+ juanitaRouter 
+};
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const lesson1Controllers = require('./controllers/lesson1')
 
-app.get('/', (req, res) => {
-  res.send('Hello World finally!')
-})
+app.use('/juanita', lesson1Controllers.juanitaRouter);
+app.use('/bugs', lesson1Controllers.bugsRouter);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
